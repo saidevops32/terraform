1a29cee626865337c143dcd375a9773e0658f59f
saidevops32 <devopsengsai@gmail.com>
2024-02-03 13:55:01 +0530

    terraform

---
+++ locals/variables.tf
@@ -0,0 +1,22 @@
+variable "instance_names" {
+  type    = list(any)
+  default = ["mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "dispatch", "web"]
+}
+
+variable "ami_id" {
+  default = "ami-0f3c7d07486cad139"
+}
+
+variable "zone_id" {
+  default = "Z00188651F95WS0TNGL6D"
+}
+
+variable "domain_name" {
+  default = "saidev.online"
+}
+
+# variable "isProd" {
+#   type = bool
+#   default = true
+# }
+
